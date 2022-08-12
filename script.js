5e0590b7b2df5d3844979aee36e69e4d12e25ab4
jjav <razornice@gmail.com>
2022-08-12 14:34:28 -0300

    Rounds playable. alerts if win o loose now need to play 5 continus rounds

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-
+let playerSelection;
+let pcSelection;
 //==============================================================================
 
 function playerPlay (e){
-let playerSelection=e.target.textContent.toLowerCase();
+playerSelection=e.target.textContent.toLowerCase();
 console.log('Your hand shows a ' + playerSelection)
 return playerSelection;
 }
@@ -11,7 +12,7 @@ return playerSelection;
 
  function pcPlay(){
     const pcOptions=["rock","paper","scissors"];
-    let pcSelection = pcOptions[Math.floor(Math.random() * pcOptions.length)];
+    pcSelection = pcOptions[Math.floor(Math.random() * pcOptions.length)];
     console.log('PC\'s hand shows a ' + pcSelection);
     return pcSelection;
   }
@@ -20,24 +21,23 @@ return playerSelection;
 //------------------------------------------------------------------------------
     
 
-function playRound(playerSelection, pcSelection){
+function playRound(){
   switch (true) {
     case playerSelection === pcSelection:
-      console.log("Its a tie! Try one more time!");
-      ties++
+      alert("DRAW!!");
       break;
 
     case playerSelection == "paper" && pcSelection == "rock":
     case playerSelection == "rock" && pcSelection == "scissors":
     case playerSelection == "scissors" && pcSelection == "paper":
-      console.log("Good job! Keep on track");
+      alert('YOU WON THE ROUND')
       wins++;
       break;
 
     case playerSelection == "rock" && pcSelection == "paper":
     case playerSelection == "paper" && pcSelection == "scissors":
     case playerSelection == "scissors" && pcSelection == "rock":
-      console.log("crap!!, but its not over");
+      alert('YOU LOST THE ROUND')
       looses++;
       break;
 
@@ -53,6 +53,6 @@ rpsbtn.forEach((button)=> {
   button.addEventListener('click', (e)=>{
     playerPlay(e);
     pcPlay();
-    playRound(playerSelection,pcSelection)
+    playRound();
   } )
 })
